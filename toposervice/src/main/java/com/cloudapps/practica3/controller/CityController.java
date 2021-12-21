166e83e60411798fd3b5031a046fde49012a8fd7
rafa <rafael.gomez@gloval.es>
2021-12-22 00:32:06 +0100

    actu

--- toposervice/src/main/java/com/cloudapps/practica3/controller/CityController.java
+++ toposervice/src/main/java/com/cloudapps/practica3/controller/CityController.java
@@ -1,14 +1,10 @@
 package com.cloudapps.practica3.controller;
 
-import java.util.Random;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.cloudapps.practica3.model.City;
 import com.cloudapps.practica3.service.CityService;
