4c39d421fb3ecc07e7e1b25f101cb79fa9997149
rafa <rafael.gomez@gloval.es>
2021-12-22 21:58:06 +0100

    cambio puerto

--- server/src/services/toposervice.js
+++ server/src/services/toposervice.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function (city){
     try {
-        const response = await axios.get('http://localhost:8080/api/topographicdetails/'+city);
+        const response = await axios.get('http://localhost:8089/api/topographicdetails/'+city);
         console.log("TopoService Response: "+response.data.landscape);
         return response.data.landscape;
       } catch (error) {
